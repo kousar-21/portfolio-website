74bb190a0af61164db47bb55f5a4f79d4a4702ee
kousar-21 <kousarahammed5@gmail.com>
2025-09-09 19:07:52 +0600

    channges in about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
                             a real impact.
                         </p>
                         <p className="text-gray-300">
-                            Let's create something amazing together 🚀
+                            Let's create something amazing together.
                         </p>
                     </p>
                 </div>
